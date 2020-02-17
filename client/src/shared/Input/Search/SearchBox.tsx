55b54ab76bf0013e96ab16915e7a4ed19eff37d3
Brett Zelk <brett.zelk@ruralsourcing.com>
2020-02-17 11:47:23 -0600

    ISSUE #29 | Adds volunteer cards for the user landing page

--- client/src/shared/Input/Search/SearchBox.tsx
+++ client/src/shared/Input/Search/SearchBox.tsx
@@ -5,37 +5,52 @@ import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useSearchStyles = makeStyles({
-  root: {
-    backgroundColor: '#FFFFFF',
-    borderRadius: '20px',
-    padding: '0px 5px'
-  },
   inputRoot: {
     height: '35px'
   }
 });
 
+const useTextFieldStyles = makeStyles({
+  root: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: '20px',
+    border: '1px solid #9A9A9A',
+    width: '100%',
+    marginRight: '30px'
+  }
+});
+
+const useInputAdornmentStyles = makeStyles({
+  root: {
+    paddingLeft: '5px'
+  }
+});
+
 const SearchBox: React.FC = props => {
   const searchStyles = useSearchStyles();
+  const textFieldStyles = useTextFieldStyles();
+  const inputAdornmentStyles = useInputAdornmentStyles();
 
   return (
     <Autocomplete
       freeSolo
-      id='free-solo-2-demo'
+      id="free-solo-2-demo"
       disableClearable
       classes={{ inputRoot: searchStyles.inputRoot }}
       renderInput={(params: any) => (
         <TextField
           {...params}
-          margin='normal'
-          fullWidth
-          classes={{ root: searchStyles.root }}
+          margin="normal"
+          classes={{ root: textFieldStyles.root }}
           InputProps={{
             ...params.InputProps,
             type: 'search',
             disableUnderline: true,
             startAdornment: (
-              <InputAdornment position='start'>
+              <InputAdornment
+                position="start"
+                classes={{ root: inputAdornmentStyles.root }}
+              >
                 <SearchIcon />
               </InputAdornment>
             )
