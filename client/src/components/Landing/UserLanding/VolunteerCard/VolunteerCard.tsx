c42d7f7c5ca6f85b27d9aec8fd0801c63024435a
Michael Harris <michaelharris72401@gmail.com>
2020-03-02 05:08:23 -0600

    Fixes crown image alt attribute

--- client/src/components/Landing/UserLanding/VolunteerCard/VolunteerCard.tsx
+++ client/src/components/Landing/UserLanding/VolunteerCard/VolunteerCard.tsx
@@ -26,7 +26,7 @@ const VolunteerCard: React.FC<IVolunteerCard> = props => {
           alignItems='center'
         >
           <Grid item xs={6} sm={2}>
-            <img src={crownImage} alt='Volunteer'/>
+            <img src={crownImage} alt=''/>
           </Grid>
           <Grid item xs={6} sm={5}>
             <div>
