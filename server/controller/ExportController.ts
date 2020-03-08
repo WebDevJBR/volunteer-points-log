379640d1cf152da415c02a4b73ba02a70a64bf2a
AustinKP <austin@DESKTOP-HDNLOPA>
2020-03-08 15:28:50 -0500

    Used prettier on new controllers.

--- server/controller/ExportController.ts
+++ server/controller/ExportController.ts
@@ -1,7 +1,7 @@
-import { Response } from "express";
-import { getManager, Repository, Like} from "typeorm";
+import { Response } from 'express';
+import { getManager, Repository, Like } from 'typeorm';
 import * as json2csv from 'json2csv';
-import { HttpStatusCodes } from "../constants/HttpStatusCodes";
+import { HttpStatusCodes } from '../constants/HttpStatusCodes';
 import { Kingdom } from '../entity/Kingdom';
 import { Volunteer } from '../entity/Volunteer';
 import { VolunteerTimeEntry } from '../entity/VolunteerTimeEntry';
@@ -12,180 +12,216 @@ import { LocalGroup } from '../entity/LocalGroup';
  * Handles calls from the 'Export' route.
  */
 export class ExportController {
-  
   /**
    * Export kingdoms and total hours per kingdom.
    * @param request The HTTP request.
    * @param response The HTTP response.
    */
-  static async exportKingdomHours(request: any, response: Response): Promise<void> {
-    
-    const kingdomRepo: Repository<Kingdom> = getManager().getRepository(Kingdom);
-   
-    // Grab data from kingdom, vols, time entries
-    let kingdomsWithVolunteerTime = await kingdomRepo
-        .find({ where: {}, 
-                join: {
-                        alias: 'kingdom', 
-                        leftJoinAndSelect: { 
-                            "volunteers": "kingdom.volunteers", 
-                            "timeEntries": "volunteers.timeEntries"
-                        }
-                }
-        });
+  static async exportKingdomHours(
+    request: any,
+    response: Response
+  ): Promise<void> {
+    const kingdomRepo: Repository<Kingdom> = getManager().getRepository(
+      Kingdom
+    );
 
-    
+    // Grab data from kingdom, vols, time entries
+    let kingdomsWithVolunteerTime = await kingdomRepo.find({
+      where: {},
+      join: {
+        alias: 'kingdom',
+        leftJoinAndSelect: {
+          volunteers: 'kingdom.volunteers',
+          timeEntries: 'volunteers.timeEntries'
+        }
+      }
+    });
+
     let csv_data = [];
     let totalActual: number = 0;
     let totalEarned: number = 0;
     kingdomsWithVolunteerTime.forEach(kingdom => {
-
-        let kingdomActualHours: number = 0;
-        let kingdomEarnedHours: number  = 0;        
-        kingdom.volunteers.forEach(vol => {
-            vol.timeEntries.forEach(entry => {
-                let hourDiff = ((Math.abs(entry.timeOut.getTime() - entry.timeIn.getTime())) / 36e5);
-                kingdomActualHours +=  hourDiff;
-                kingdomEarnedHours += ( entry.multiplier != 0 ) ? entry.multiplier * hourDiff: hourDiff;
-            });
+      let kingdomActualHours: number = 0;
+      let kingdomEarnedHours: number = 0;
+      kingdom.volunteers.forEach(vol => {
+        vol.timeEntries.forEach(entry => {
+          let hourDiff =
+            Math.abs(entry.timeOut.getTime() - entry.timeIn.getTime()) / 36e5;
+          kingdomActualHours += hourDiff;
+          kingdomEarnedHours +=
+            entry.multiplier != 0 ? entry.multiplier * hourDiff : hourDiff;
         });
+      });
 
-        totalActual += kingdomActualHours;
-        totalEarned += kingdomEarnedHours;
+      totalActual += kingdomActualHours;
+      totalEarned += kingdomEarnedHours;
 
-        csv_data.push({"Kingdom of Residence": kingdom.name, "Actual Hours": kingdomActualHours?.toFixed(2), "Earned Hours": kingdomEarnedHours?.toFixed(2)});
+      csv_data.push({
+        'Kingdom of Residence': kingdom.name,
+        'Actual Hours': kingdomActualHours?.toFixed(2),
+        'Earned Hours': kingdomEarnedHours?.toFixed(2)
+      });
     });
 
     // Write total row
-    csv_data.push({"Kingdom of Residence": "Total", "Actual Hours": totalActual?.toFixed(2), "Earned Hours": totalEarned?.toFixed(2)});
-    
+    csv_data.push({
+      'Kingdom of Residence': 'Total',
+      'Actual Hours': totalActual?.toFixed(2),
+      'Earned Hours': totalEarned?.toFixed(2)
+    });
+
     let data = await json2csv.parse(csv_data);
 
     response.attachment('kingdomHours.csv');
     response.set({
-        'Content-Type': 'text/csv',
-        'Content-Disposition': 'attachment; filename=kingdomAndGroups.csv',
-        'Pragma': 'no-cache',
-        'Expires': '0'
+      'Content-Type': 'text/csv',
+      'Content-Disposition': 'attachment; filename=kingdomAndGroups.csv',
+      Pragma: 'no-cache',
+      Expires: '0'
     });
     response.status(HttpStatusCodes.Ok).send(data);
   }
-
 
   /**
    * Export a more specific breakdown of each kingdom and their local groups.
    * @param request The HTTP request.
    * @param response The HTTP response.
    */
-  static async exportKingdomBreakdown(request: any, response: Response): Promise<void> {
-    
-    const kingdomRepo: Repository<Kingdom> = getManager().getRepository(Kingdom);
-    const toReceieveRepo: Repository<ToReceiveFundsType_REF> = getManager().getRepository(ToReceiveFundsType_REF);
-    const volunteerRepo: Repository<Volunteer> = getManager().getRepository(Volunteer);
-    const localGroupRepo: Repository<LocalGroup> = getManager().getRepository(LocalGroup);
+  static async exportKingdomBreakdown(
+    request: any,
+    response: Response
+  ): Promise<void> {
+    const kingdomRepo: Repository<Kingdom> = getManager().getRepository(
+      Kingdom
+    );
+    const toReceieveRepo: Repository<ToReceiveFundsType_REF> = getManager().getRepository(
+      ToReceiveFundsType_REF
+    );
+    const volunteerRepo: Repository<Volunteer> = getManager().getRepository(
+      Volunteer
+    );
+    const localGroupRepo: Repository<LocalGroup> = getManager().getRepository(
+      LocalGroup
+    );
 
     // Specific kingdom to look at
-    const kingdomId = request.query["id"] || null;
-    const kingdom = await kingdomRepo.findOne({where: {id: kingdomId}});
+    const kingdomId = request.query['id'] || null;
+    const kingdom = await kingdomRepo.findOne({ where: { id: kingdomId } });
 
     const getHours = async (toRecType: string, kingdom: Kingdom) => {
+      let toReceiveFundsTypeRef = await toReceieveRepo.findOne({
+        where: { type: Like(toRecType) }
+      });
 
-        let toReceiveFundsTypeRef = await toReceieveRepo.findOne({where: {type: Like(toRecType)}});
+      let volunteers = await volunteerRepo.find({
+        where: { toReceiveFundsType: toReceiveFundsTypeRef, kingdom: kingdom },
+        join: {
+          alias: 'volunteer',
+          leftJoinAndSelect: {
+            timeEntries: 'volunteer.timeEntries'
+          }
+        }
+      });
 
-
-        let volunteers = await volunteerRepo.find({
-            where: { toReceiveFundsType: toReceiveFundsTypeRef, kingdom: kingdom },
-            join: { 
-                alias: 'volunteer', 
-                leftJoinAndSelect: {
-                    "timeEntries": "volunteer.timeEntries"
-                }
-            } 
-        });
-
-        return volunteers;
+      return volunteers;
     };
 
     const calculateEntryHours = (timeEntries: VolunteerTimeEntry[]) => {
-        let actual = 0;
-        let earned = 0;
-        
-        timeEntries.forEach(entry => {
-            let hourDiff = ((Math.abs(entry.timeOut.getTime()- entry.timeIn.getTime())) / 36e5);
-            actual +=  hourDiff;
-            earned +=  ((hourDiff * entry.multiplier) + hourDiff);
-        })
+      let actual = 0;
+      let earned = 0;
 
-        return {actual, earned};
-    }
-    
-    
-    // Breakdown of specific hours for the kingdom 
+      timeEntries.forEach(entry => {
+        let hourDiff =
+          Math.abs(entry.timeOut.getTime() - entry.timeIn.getTime()) / 36e5;
+        actual += hourDiff;
+        earned += hourDiff * entry.multiplier + hourDiff;
+      });
+
+      return { actual, earned };
+    };
+
+    // Breakdown of specific hours for the kingdom
     let actual: number = 0;
     let earned: number = 0;
     let volSpecificEntries = await getHours('Kingdom', kingdom);
 
     // Loop through each volunteer, adding up hours for this specific kingdom entry
-    volSpecificEntries.forEach(vol => { 
-        let hours = calculateEntryHours(vol.timeEntries);
-        actual = hours.actual;
-        earned = hours.earned;
+    volSpecificEntries.forEach(vol => {
+      let hours = calculateEntryHours(vol.timeEntries);
+      actual = hours.actual;
+      earned = hours.earned;
     });
 
     // Handle header / entry for specific kingdom
     let csv_data = [];
-   
-    csv_data.push({"Kingdom": kingdom.name, "Actual Hours": 0, "Earned Hours": 0});
-    csv_data.push({"Kingdom": "To Kingdom of " + kingdom.name, "Actual Hours": actual.toFixed(2), "Earned Hours": earned.toFixed(2)});
 
+    csv_data.push({
+      Kingdom: kingdom.name,
+      'Actual Hours': 0,
+      'Earned Hours': 0
+    });
+    csv_data.push({
+      Kingdom: 'To Kingdom of ' + kingdom.name,
+      'Actual Hours': actual.toFixed(2),
+      'Earned Hours': earned.toFixed(2)
+    });
 
     // Local Groups Break down
-    let toReceiveFundsTypeRefLG = await toReceieveRepo.findOne({where: {type: Like('Local Group')}});
+    let toReceiveFundsTypeRefLG = await toReceieveRepo.findOne({
+      where: { type: Like('Local Group') }
+    });
     // Second query is get all time entries for volunteers with a local group specified
-    // TODO: Fix broken state when you change the ref value to kingdom / other. Still tallies 
+    // TODO: Fix broken state when you change the ref value to kingdom / other. Still tallies
     let localGroupEntries = await localGroupRepo.find({
-        where: { kingdom:  kingdomId, toReceiveFundsType: toReceiveFundsTypeRefLG},
-        join: { 
-            alias: 'localGroup', 
-            leftJoinAndSelect: {
-                "volunteers": "localGroup.volunteers",
-                "timeEntries": "volunteers.timeEntries"
-            }
-        } 
+      where: {
+        kingdom: kingdomId,
+        toReceiveFundsType: toReceiveFundsTypeRefLG
+      },
+      join: {
+        alias: 'localGroup',
+        leftJoinAndSelect: {
+          volunteers: 'localGroup.volunteers',
+          timeEntries: 'volunteers.timeEntries'
+        }
+      }
     });
 
     actual = earned = 0;
     localGroupEntries.forEach(lg => {
-        lg.volunteers.forEach(vol => {
-            let hours = calculateEntryHours(vol.timeEntries);
-            actual = hours.actual;
-            earned = hours.earned;
-        });
-        csv_data.push({"Kingdom": lg.name, "Actual Hours": actual.toFixed(2), "Earned Hours": earned.toFixed(2)});
+      lg.volunteers.forEach(vol => {
+        let hours = calculateEntryHours(vol.timeEntries);
+        actual = hours.actual;
+        earned = hours.earned;
+      });
+      csv_data.push({
+        Kingdom: lg.name,
+        'Actual Hours': actual.toFixed(2),
+        'Earned Hours': earned.toFixed(2)
+      });
     });
 
-    
     // Last query to get the data for volunteers who picked other for said kingdom
     // Used to verify we only pull volunteers who are using LG as indicated
     let otherEntries = await getHours('Other', kingdom);
 
     actual = earned = 0;
     otherEntries.forEach(vol => {
-        
-        let hours = calculateEntryHours(vol.timeEntries);
-        actual = hours.actual;
-        earned = hours.earned;
-       
-        csv_data.push({"Kingdom": vol.other, "Actual Hours": actual.toFixed(2), "Earned Hours": earned.toFixed(2)});
-    });
+      let hours = calculateEntryHours(vol.timeEntries);
+      actual = hours.actual;
+      earned = hours.earned;
 
+      csv_data.push({
+        Kingdom: vol.other,
+        'Actual Hours': actual.toFixed(2),
+        'Earned Hours': earned.toFixed(2)
+      });
+    });
 
     // Sum up totals
     csv_data.forEach(val => {
-        csv_data[0]["Actual Hours"] += +(val["Actual Hours"]);
-        csv_data[0]["Earned Hours"] += +(val["Earned Hours"]);
-    })
-    
+      csv_data[0]['Actual Hours'] += +val['Actual Hours'];
+      csv_data[0]['Earned Hours'] += +val['Earned Hours'];
+    });
 
     let data = await json2csv.parse(csv_data);
     response.attachment('kingdomBreakdown.csv');
--- server/controller/ImportController.ts
+++ server/controller/ImportController.ts
@@ -1,45 +1,48 @@
-import { Response } from "express";
-import { getManager, Repository, Like } from "typeorm";
-import * as csv from "csv-parser";
-import * as fs from "fs";
-import { HttpStatusCodes } from "../constants/HttpStatusCodes";
+import { Response } from 'express';
+import { getManager, Repository, Like } from 'typeorm';
+import * as csv from 'csv-parser';
+import * as fs from 'fs';
+import { HttpStatusCodes } from '../constants/HttpStatusCodes';
 import { Kingdom } from '../entity/Kingdom';
 import { Volunteer } from '../entity/Volunteer';
 import { ToReceiveFundsType_REF } from '../entity/ToReceiveFundsType_REF';
 import { LocalGroup } from '../entity/LocalGroup';
-import { Department } from "../entity/Department";
-
+import { Department } from '../entity/Department';
 
 /**
  * Handles calls from the 'imports' route.
  */
 export class ImportController {
-  
   /**
    * Imports kingdoms and respected local groups.
    * @param request The HTTP request.
    * @param response The HTTP response.
    */
-  static async importKingdomsAndGroups(request: any, response: Response): Promise<void> {
-    
-    const kingdomRepo: Repository<Kingdom> = getManager().getRepository(Kingdom);
-    const localGroupRepo: Repository<LocalGroup> = getManager().getRepository(LocalGroup);
-    
+  static async importKingdomsAndGroups(
+    request: any,
+    response: Response
+  ): Promise<void> {
+    const kingdomRepo: Repository<Kingdom> = getManager().getRepository(
+      Kingdom
+    );
+    const localGroupRepo: Repository<LocalGroup> = getManager().getRepository(
+      LocalGroup
+    );
+
     let rows: any[] = [];
     fs.createReadStream(request.file.path)
       .pipe(csv())
-      .on("data", row => {
+      .on('data', row => {
         rows.push(row);
       })
-      .on("end", async () => {
+      .on('end', async () => {
         fs.unlinkSync(request.file.path);
 
         // Import kingdoms (Stripping off any whitespace from strings)
         const kingdoms = Object.keys(rows[0]);
         kingdoms.map(kingdom => kingdom.trim());
-        
+
         for (let kingdom of kingdoms) {
-          
           let existingKingdom: Kingdom = await kingdomRepo.findOne({
             where: { name: Like(kingdom) }
           });
@@ -55,7 +58,6 @@ export class ImportController {
         // Import local groups
         for (let row of rows) {
           for (let [kingdom, group] of Object.entries(row)) {
-            
             let existingKingdom: Kingdom = await kingdomRepo.findOne({
               where: { name: Like(kingdom) }
             });
@@ -80,34 +82,64 @@ export class ImportController {
       });
   }
 
-
-   /**
+  /**
    * Imports volunteers.
    * @param request The HTTP request.
    * @param response The HTTP response.
    */
-  static async importVolunteers(request: any, response: Response): Promise<void> {
-    
-    const kingdomRepo: Repository<Kingdom> = getManager().getRepository(Kingdom);
-    const localGroupRepo: Repository<LocalGroup> = getManager().getRepository(LocalGroup);
-    const volunteerRepo: Repository<Volunteer> = getManager().getRepository(Volunteer);
-    const receieveFundsTypeRepo: Repository<ToReceiveFundsType_REF> = getManager().getRepository(ToReceiveFundsType_REF);
+  static async importVolunteers(
+    request: any,
+    response: Response
+  ): Promise<void> {
+    const kingdomRepo: Repository<Kingdom> = getManager().getRepository(
+      Kingdom
+    );
+    const localGroupRepo: Repository<LocalGroup> = getManager().getRepository(
+      LocalGroup
+    );
+    const volunteerRepo: Repository<Volunteer> = getManager().getRepository(
+      Volunteer
+    );
+    const receieveFundsTypeRepo: Repository<ToReceiveFundsType_REF> = getManager().getRepository(
+      ToReceiveFundsType_REF
+    );
 
     let rows: number = 0;
     fs.createReadStream(request.file.path)
-      .pipe(csv({headers: ['timestamp', 'email', 'mundane', 'sca', 'kingdom', 'localGroup', 'toReceive', 'membershipNum' ]}))
-      .on("data", async (vol) => {
-
+      .pipe(
+        csv({
+          headers: [
+            'timestamp',
+            'email',
+            'mundane',
+            'sca',
+            'kingdom',
+            'localGroup',
+            'toReceive',
+            'membershipNum'
+          ]
+        })
+      )
+      .on('data', async vol => {
         // Required to remove trailing whitespace in the csv cell
-        Object.keys(vol).map((key, index) => { vol[key] = (typeof vol[key] === 'string') ?  vol[key].trim() : vol[key]});
+        Object.keys(vol).map((key, index) => {
+          vol[key] = typeof vol[key] === 'string' ? vol[key].trim() : vol[key];
+        });
 
         // Required to include header row in csv file, but skips over it
         if (rows > 0) {
-          
-          let existingVolunteer: Volunteer = await volunteerRepo.findOne({ name: vol.sca });
-          let existingKingdom: Kingdom = await kingdomRepo.findOne({name: vol.kingdom});
-          let existingGroup: LocalGroup = await localGroupRepo.findOne({ name: vol.localGroup});
-          let fundsType: ToReceiveFundsType_REF = await receieveFundsTypeRepo.findOne({where: { type: Like('Local Group')}});
+          let existingVolunteer: Volunteer = await volunteerRepo.findOne({
+            name: vol.sca
+          });
+          let existingKingdom: Kingdom = await kingdomRepo.findOne({
+            name: vol.kingdom
+          });
+          let existingGroup: LocalGroup = await localGroupRepo.findOne({
+            name: vol.localGroup
+          });
+          let fundsType: ToReceiveFundsType_REF = await receieveFundsTypeRepo.findOne(
+            { where: { type: Like('Local Group') } }
+          );
 
           let newVolunteer = new Volunteer();
           newVolunteer.mka = vol.mundane ? vol.mundane : '';
@@ -115,51 +147,55 @@ export class ImportController {
           newVolunteer.kingdom = existingKingdom?.id;
           newVolunteer.localGroup = existingGroup?.id;
           newVolunteer.membershipNumber = vol.membershipNum;
-          newVolunteer.infoMissing = (!existingKingdom || !existingGroup) ? true: false;
+          newVolunteer.infoMissing =
+            !existingKingdom || !existingGroup ? true : false;
 
-         // Handles to receieve cases
+          // Handles to receieve cases
           if (vol.toReceive) {
-            if ((vol.toReceive == vol.localGroup) && existingGroup) {
+            if (vol.toReceive == vol.localGroup && existingGroup) {
               newVolunteer.toReceiveFundsType = fundsType?.id;
-            }
-            else if ((vol.toReceive == vol.kingdom) && existingKingdom) {
-              fundsType  = await receieveFundsTypeRepo.findOne({where:{type:Like('Kingdom')}});
+            } else if (vol.toReceive == vol.kingdom && existingKingdom) {
+              fundsType = await receieveFundsTypeRepo.findOne({
+                where: { type: Like('Kingdom') }
+              });
               newVolunteer.toReceiveFundsType = fundsType?.id;
-            }
-            else {
+            } else {
               newVolunteer.other = vol.toReceive;
-              fundsType  = await receieveFundsTypeRepo.findOne({where:{type:Like('Other')}});
+              fundsType = await receieveFundsTypeRepo.findOne({
+                where: { type: Like('Other') }
+              });
               newVolunteer.toReceiveFundsType = fundsType?.id;
             }
-          } 
-          
+          }
+
           try {
             if (existingVolunteer) {
               // If kingdom ids match update the exisiting volunteer (Same logic as new, but updating existing)
               if (existingVolunteer.kingdom === existingKingdom?.id) {
-               
                 existingVolunteer.mka = vol.mundane ? vol.mundane : '';
                 existingVolunteer.name = vol.sca ? vol.sca : '';
                 existingVolunteer.localGroup = existingGroup?.id;
                 existingVolunteer.membershipNumber = vol.membershipNum;
-                existingVolunteer.infoMissing = (!existingGroup) ? true: false;
+                existingVolunteer.infoMissing = !existingGroup ? true : false;
 
                 // Handles to receieve cases
                 if (vol.toReceive) {
                   if (vol.toReceive == existingGroup?.name) {
                     existingVolunteer.toReceiveFundsType = fundsType?.id;
-                  }
-                  else if (vol.toReceive == existingGroup?.name) {
-                    fundsType  = await receieveFundsTypeRepo.findOne({where:{type:Like('Kingdom')}});
+                  } else if (vol.toReceive == existingGroup?.name) {
+                    fundsType = await receieveFundsTypeRepo.findOne({
+                      where: { type: Like('Kingdom') }
+                    });
                     existingVolunteer.toReceiveFundsType = fundsType?.id;
-                  }
-                  else {
+                  } else {
                     existingVolunteer.other = vol.toReceive;
-                    fundsType  = await receieveFundsTypeRepo.findOne({where:{type:Like('Other')}});
+                    fundsType = await receieveFundsTypeRepo.findOne({
+                      where: { type: Like('Other') }
+                    });
                     existingVolunteer.toReceiveFundsType = fundsType?.id;
                   }
-                } 
-  
+                }
+
                 await volunteerRepo.save(existingVolunteer);
               }
               // Otherwise replace it
@@ -168,54 +204,61 @@ export class ImportController {
                 newVolunteer.infoMissing = true;
                 await volunteerRepo.save(newVolunteer);
               }
-            } 
-            else {
+            } else {
               if (newVolunteer.mka && newVolunteer.name) {
                 await volunteerRepo.save(newVolunteer);
               }
             }
-            
-          }
-          catch(err) {
+          } catch (err) {
             console.log(err);
-            console.log("Error creating volunteer: ", vol);
+            console.log('Error creating volunteer: ', vol);
           }
         }
-          
+
         rows++;
       })
-      .on("end", async () => {
-        fs.unlinkSync(request.file.path);    
+      .on('end', async () => {
+        fs.unlinkSync(request.file.path);
         response.sendStatus(HttpStatusCodes.Created);
       });
   }
-
 
   /**
    * Imports volunteers.
    * @param request The HTTP request.
    * @param response The HTTP response.
    */
-  static async importDepartments(request: any, response: Response): Promise<void> {
-    
-    const departmentRepo: Repository<Department> = getManager().getRepository(Department);
-    const volunteerRepo: Repository<Volunteer> = getManager().getRepository(Volunteer);
-   
+  static async importDepartments(
+    request: any,
+    response: Response
+  ): Promise<void> {
+    const departmentRepo: Repository<Department> = getManager().getRepository(
+      Department
+    );
+    const volunteerRepo: Repository<Volunteer> = getManager().getRepository(
+      Volunteer
+    );
 
     let rows: number = 0;
     fs.createReadStream(request.file.path)
-      .pipe(csv({headers: ['department', 'head', 'deputy' ]}))
-      .on("data", async (row) => {
-
+      .pipe(csv({ headers: ['department', 'head', 'deputy'] }))
+      .on('data', async row => {
         // Required to remove trailing whitespace in the csv cell
-        Object.keys(row).map((key, index) => { row[key] = (typeof row[key] === 'string') ?  row[key].trim() : row[key]});
+        Object.keys(row).map((key, index) => {
+          row[key] = typeof row[key] === 'string' ? row[key].trim() : row[key];
+        });
 
         // Required to include header row in csv file, but skips over it
         if (rows > 0) {
-          
-          let existingDepartment: Department = await departmentRepo.findOne({ name: row.department });
-          let exisitingHead: Volunteer = await volunteerRepo.findOne({name: row.head});
-          let exisitingDeputy: Volunteer = await volunteerRepo.findOne({name: row.deputy});
+          let existingDepartment: Department = await departmentRepo.findOne({
+            name: row.department
+          });
+          let exisitingHead: Volunteer = await volunteerRepo.findOne({
+            name: row.head
+          });
+          let exisitingDeputy: Volunteer = await volunteerRepo.findOne({
+            name: row.deputy
+          });
 
           // Update exisiting
           if (existingDepartment) {
@@ -223,10 +266,11 @@ export class ImportController {
             existingDepartment.deputyVolunteer = exisitingDeputy?.id;
 
             await departmentRepo.save(existingDepartment);
-          }
-          else {
+          } else {
             let newDepartment = new Department();
-            newDepartment.name = row.department ? row.department: newDepartment.name;
+            newDepartment.name = row.department
+              ? row.department
+              : newDepartment.name;
             newDepartment.headVolunteer = exisitingHead?.id;
             newDepartment.deputyVolunteer = exisitingDeputy?.id;
 
@@ -237,8 +281,8 @@ export class ImportController {
         }
         rows++;
       })
-      .on("end", async () => {
-        fs.unlinkSync(request.file.path);    
+      .on('end', async () => {
+        fs.unlinkSync(request.file.path);
         response.sendStatus(HttpStatusCodes.Created);
       });
   }
